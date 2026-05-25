04a20420fa1fc6f6ddca6e0c3b8ebff219b7a6d2
Shay <shay.k@whatson-pay.com>
2026-05-25 17:14:29 +0300

    added axios to forms

---
+++ react-forms/src/lib/axiosInstance.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const baseUrl = "http://localhost:8080";
+const axiosInstance = axios.create({
+    baseURL: baseUrl,
+    withCredentials: false,
+});
+// Method GET, POST, PUT, DELETE, PATCH
+export const apiRequest = async ({
+                                     method,
+                                     path,
+                                     params = {},
+                                     data = {},
+                                     headers = {}
+                                 }) => {
+    try {
+        const response = await axiosInstance({
+            method,
+            url: path,
+            params,
+            data,
+            headers
+        })
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+
+}
+// export const axiosConfig = {
+//     baseURL: url.toString(),
+// }
+//
+// axios.defaults.withCredentials = false;
+// export const axiosInstance = axios.create(axiosConfig);
